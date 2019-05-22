9dca25cd59de63c58e3595349347b5cc7779a61d
Andrew Jones <andrew@andrewjones.id.au>
2019-05-23 08:55:51 +1000

    Add initial keymap.

---
+++ config.h
@@ -0,0 +1,8 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef ONESHOT_TIMEOUT
+#define ONESHOT_TIMEOUT 1077
+
+#define AUTO_SHIFT_TIMEOUT 158
